0c37181a821e76c23f59e60701dce937cdcc59ca
madhankumar028 <madhankumar028@gmail.com>
2020-03-21 12:22:59 +0700

    adds core scss 7-1 pattern.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import './core/styles/app.scss';
 
 function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <h4>Covid-19 Real time data</h4>
+        <h4 className="heading-4">Covid-19 Real time data</h4>
       </header>
     </div>
   );
