14eaf8d4cc8543468791bea717395a1d9e52f397
madhankumar028 <madhankumar028@gmail.com>
2020-03-21 16:45:19 +0700

    adds all and bycountry events.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import PieChart from './core/components/chart/chart';
+import EventHandler from './event';
 
 import './core/styles/app.scss';
 
 function App() {
+
+  const [countryList, setCountryList] = useState([]);
+
+  useEffect(() => {
+    EventHandler({eventName: 'byCountry', options: { country: 'india', status: 'confirmed' }})
+      .then(res => {
+        console.log(res);
+        setCountryList(res);
+      });
+  }, []);
+
   return (
     <div className="app">
       <header className="app-header">
         <h4 className="heading-4">Covid-19 Real time data</h4>
-        <PieChart />
+        {
+          countryList.length > 0 &&
+            <PieChart dataList={countryList} />
+        }
       </header>
     </div>
   );
