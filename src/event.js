53c021773bcba1e8e969a345864419c8c8354a4a
harishkar <hmohan_c@rpxcorp.com>
2020-03-21 15:21:26 +0530

    merge master

---
+++ src/event.js
@@ -0,0 +1,33 @@
+const BASE_URL = 'https://api.covid19api.com';
+
+const EventHanlder = ({eventName, options}) => {
+	const event = {
+		all: () => {
+			return (
+				fetch(`${BASE_URL}/all`)
+				.then(res => {
+					if (res.ok) {
+						return res;
+					} else {
+						throw new Error('Something went wrong');
+					}
+				})
+			)
+		},
+		byCountry: ({country, status}) => {
+			return (
+				fetch(`${BASE_URL}/total/country/${country}/status/${status}`)
+				.then(res => {
+					if (res.ok) {
+						return res.json();
+					} else {
+						throw new Error('Something went wrong');
+					}
+				})
+			)
+		}
+	};
+	return event[eventName](options);
+};
+
+export default EventHanlder;
