5465dad4c5d7497737c6050a75f6dc0c3a9b52a7
madhankumar028 <madhankumar028@gmail.com>
2020-03-21 16:56:25 +0700

    removes extra imports and app-header.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import EventHandler from './event';
 
 import './core/styles/app.scss';
 import Navbar from './core/components/navbar/navbar';
-import PieChart from './core/components/chart/chart';
 
 function App() {
 
@@ -23,7 +22,6 @@ function App() {
     <div className="app">
       <Navbar name="Covid-19 Worldwide Live update"/>
       <header className="app-header">
-        <h4 className="heading-4">Covid-19 Real time data</h4>
         {
           countryList.length > 0 &&
             <PieChart dataList={countryList} />
