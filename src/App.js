c5e0b66ba766c0f564f6503f9dcf948001182823
Harishkar M <3harish.m@gmail.com>
2020-03-21 15:24:35 +0530

    Dev (#1)
    * favicon and title changed
    
    * nav bar added
    
    * nav bar added
    
    * merge master
    
    Co-authored-by: harishkar <hmohan_c@rpxcorp.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import PieChart from './core/components/chart/chart';
 import EventHandler from './event';
 
 import './core/styles/app.scss';
+import Navbar from './core/components/navbar/navbar';
+import PieChart from './core/components/chart/chart';
 
 function App() {
 
@@ -19,6 +21,7 @@ function App() {
 
   return (
     <div className="app">
+      <Navbar name="Covid-19 Worldwide Live update"/>
       <header className="app-header">
         <h4 className="heading-4">Covid-19 Real time data</h4>
         {
